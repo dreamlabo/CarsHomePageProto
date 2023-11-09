2178dcf1489fbc3572a47c2fd2e5dc23378aa1ae
dreamlabo <toddlabo@gmail.com>
2023-11-09 09:29:23 -0700

    Working through events page.

--- scripts.js
+++ scripts.js
@@ -3,32 +3,60 @@ let isMobileMenuOpen = false;
 const mobileMenu = document.getElementById("mobile-menu");
 const mainPageWrapper = document.getElementById("main-page-wrapper")
 const mobileNavLinks = document.getElementsByClassName("mobile-nav-link");
+const lastFocusElem = mobileNavLinks[mobileNavLinks.length - 1].getElementsByTagName('a')[0]
+const firstFocusElem = mobileMenuBtn;
 
 mobileMenuBtn.addEventListener('click', handleMobileMenuBtn)
 
 function handleMobileMenuBtn(event) {
-    console.log('clicked')
-
     isMobileMenuOpen ? closeMobileMenu() : openMobileMenu();
 }
 
 
 function closeMobileMenu() {
+    mobileMenuBtn.setAttribute('aria-expanded', "false");
+    mobileMenu.setAttribute('aria-hidden', "true");
     isMobileMenuOpen = false;
     mobileMenu.classList.remove("menu-open")
     mobileMenuBtn.classList.remove("active");
     mainPageWrapper.style.zIndex = "1"
-    console.log('clicked close')
+    for(i of mobileNavLinks){
+        i.firstChild.tabIndex = -1;
+   }
 
+   document.removeEventListener('keydown', navigateMenu)
 }
 
 function openMobileMenu() {
-    mobileNavLinks[0].focus();
+    mobileMenuBtn.setAttribute('aria-expanded', "true");
+    mobileMenu.setAttribute('aria-hidden', "false");
     isMobileMenuOpen = true
     mobileMenu.classList.add("menu-open")
-    console.log('clicked open')
     mobileMenuBtn.classList.add("active");
     mainPageWrapper.style.zIndex = "-1"
+    
+   for(i of mobileNavLinks){
+        i.firstChild.tabIndex = 0;
+   }
+
+   document.addEventListener('keydown', navigateMenu)
+}
+
+function navigateMenu(e) {
+    console.log(e.key)
+
+    if (e.key === 'Tab') {
+        // If Shift key is also held, move focus backward
+        if (e.shiftKey && document.activeElement === firstFocusElem) {
+          e.preventDefault();
+          lastFocusElem.focus();
+        }
+        // If only Tab key is pressed, move focus forward
+        else if (!e.shiftKey && document.activeElement === lastFocusElem) {
+            e.preventDefault();
+            firstFocusElem.focus();
+        }
+      }
 }
 
 
---
+++ scripts/eventsScripts.js
@@ -0,0 +1,46 @@
+const events = [
+    {
+        headliner: "The Musical Box",
+        supportActs: [],
+        date: "Fri, Dec 8, 2023",
+        time: "8:00pm",
+        venue: "Paramount Theater",
+        city: "Denver",
+        state: "Co",
+        ticketURL: "https://www.ticketmaster.com/the-musical-box-denver-colorado-12-08-2023/event/1E005EFF907E1B43?camefrom=CFC_KSE_gbjbnveBcU69DVgYv0JfZg&utm_source=gbjbnveBcU69DVgYv0JfZg&utm_medium=gbjbnveBcU69DVgYv0JfZg&utm_campaign=gbjbnveBcU69DVgYv0JfZg",
+        bandInfo: "https://www.themusicalbox.net/"
+    },
+    {
+        headliner: "Haken",
+        supportActs: [],
+        date: "Thur, Feb 15, 2024",
+        time: "8:00pm",
+        venue: "Boulder Theater",
+        city: "Boulder",
+        state: "Co",
+        ticketURL: "https://www.axs.com/events/502001/haken-tickets",
+        bandInfo: "https://hakenmusic.com/"
+    },
+    {
+        headliner: "The Ocean",
+        supportActs: ["The World Is A Beautiful Place & I Am No Longer Afraid To Die"],
+        date: "Thur, Feb 15, 2024",
+        time: "8:00pm",
+        venue: "Boulder Theater",
+        city: "Boulder",
+        state: "Co",
+        ticketURL: "https://www.axs.com/events/510028/the-ocean-tickets?q=the+Ocean",
+        bandInfo: "https://www.theoceancollective.com/"
+    },
+    {
+        headliner: "Soen",
+        supportActs: [],
+        date: "Sun, June 9, 2024",
+        time: "7:30pm",
+        venue: "Bluebird Theater",
+        city: "Denver",
+        state: "Co",
+        ticketURL: "https://www.axs.com/events/498617/soen-tickets",
+        bandInfo: "https://soenmusic.com/"
+    }
+]
