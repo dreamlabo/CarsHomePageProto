983c6d763e0157685266183edcb52d5c6914ef46
dreamlabo <toddlabo@gmail.com>
2024-01-05 21:37:11 -0700

    added progjam poster

--- scripts/meetingsInfo.js
+++ scripts/meetingsInfo.js
@@ -1,14 +1,10 @@
 meetings = [
     {
-        date: new Date('2023/12/9'),
+        date: new Date('2024/1/27'),
         time: '6:00pm',
         host: 'Kent',
         hostEmail: 'kentsEmail@mail.com',
-        additionalInfo: [
-                        "This month's meeting is our annual holiday party.", 
-                        "It's a holiday potluck and BYOB, so please bring whatever you'd like to drink, along with a side dish, salad or dessert to share. Kent is providing a holiday ham.",
-                        "If you want to participate in the gift exchange bring a music-related wrapped gift (CDs, books, etc.)."
-                    ]
+        additionalInfo: []
     },
     {
         date: new Date('2023/11/11'),
@@ -20,11 +16,15 @@ meetings = [
 ]
 
 function returnUpcomingMeeting() {
-    const currentDate = new Date();
+
+    var currentDate = new Date(new Date().toDateString()); 
+    
     const currentDateFormatted = currentDate.toLocaleDateString('en-US', {  timeZone: 'America/Denver' });
     
     return meetings.find(meeting => {
+        var date2 = new Date(new Date(meeting.date).toDateString()); 
         const meetingDateFormatted = meeting.date.toLocaleDateString('en-US', {  timeZone: 'America/Denver' });
-        return meetingDateFormatted >= currentDateFormatted;
+        return date2 >= currentDate;
       });
 }
+
